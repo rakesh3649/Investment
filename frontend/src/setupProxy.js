454f7b8fe9adc55a1a86eaeafb5a5a717f7fe121
Rakesh <rakeshmgs40@gmail.com>
2026-07-13 17:34:06 +0530

    api change

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: 'https://investment-v9as.onrender.com',
       changeOrigin: true,
       logLevel: 'debug'
     })
